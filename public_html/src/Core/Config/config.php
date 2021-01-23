66d34ec69b74d1862e25671e6b6c1b84883aa8b6
kristaps.taube <kristaps.taube@printful.com>
2021-01-23 21:04:58 +0200

    gitignore update

---
+++ public_html/src/Core/Config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+$localConfig = require_once 'config-local.php';
+
+return $localConfig + [
+        'templatePath' => APP_ROOT . '/src/Frontend/Templates',
+        'database' => [
+            'host' => '',
+            'port' => '',
+            'name' => '',
+            'username' => '',
+            'password' => '',
+        ],
+        'router' => [
+            'configurators' => [
+                \OSM\Frontend\Routes\SiteRouteConfigurator::class,
+            ],
+        ],
+        'command' => [
+            'configurators' => [
+                \OSM\Console\Commands\SiteCommandConfigurator::class,
+            ],
+        ],
+    ];
