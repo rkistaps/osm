6dfeb080956412fb7f4ffdbf1be4545d9ccac653
kristaps.taube <kristaps.taube@printful.com>
2021-04-17 19:48:41 +0300

    remove unused dependencies

--- public_html/src/Console/Handlers/Series/Leagues/LeagueRoundRunnerHandler.php
+++ public_html/src/Console/Handlers/Series/Leagues/LeagueRoundRunnerHandler.php
@@ -4,27 +4,19 @@ declare(strict_types=1);
 
 namespace OSM\Console\Handlers\Series\Leagues;
 
-use OSM\Core\Repositories\ChampionshipLeagueRepository;
-use OSM\Core\Repositories\ChampionshipRepository;
 use OSM\Modules\Series\Leagues\Services\LeagueRunnerService;
 use Psr\Log\LoggerInterface;
 
 class LeagueRoundRunnerHandler implements \TheApp\Interfaces\CommandHandlerInterface
 {
     private LeagueRunnerService $leagueRunnerService;
-    private ChampionshipLeagueRepository $leagueRepository;
-    private ChampionshipRepository $championshipRepository;
     private LoggerInterface $logger;
 
     public function __construct(
         LoggerInterface $logger,
         LeagueRunnerService $leagueRunnerService,
-        ChampionshipLeagueRepository $leagueRepository,
-        ChampionshipRepository $championshipRepository
     ) {
         $this->leagueRunnerService = $leagueRunnerService;
-        $this->leagueRepository = $leagueRepository;
-        $this->championshipRepository = $championshipRepository;
         $this->logger = $logger;
     }
 
