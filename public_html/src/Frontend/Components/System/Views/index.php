f3fd099e66ec2916190e60b7e35ed8d5b41ee4a3
kristaps.taube <kristaps.taube@printful.com>
2021-04-23 21:59:13 +0300

    System component wip

--- public_html/src/Frontend/Components/System/Views/index.php
+++ public_html/src/Frontend/Components/System/Views/index.php
@@ -8,6 +8,6 @@ use OSM\Frontend\Helpers\Html;
 <div id='system'>
     <?= _d("frontend", "Server time") ?>: <?= $state->getCurrentDateTime() ?>.
     <?= _d("frontend", "Online") ?>:
-    <?= Html::a(str_replace("[count]", 0, _d("frontend", "[count] users")), '/online') ?>
-    (<?= Html::a(0, '/new-users') ?>)
+    <?= Html::a(str_replace("[count]", '???', _d("frontend", "[count] users")), '/online') ?>
+    (<?= Html::a('???', '/new-users') ?>)
 </div>
