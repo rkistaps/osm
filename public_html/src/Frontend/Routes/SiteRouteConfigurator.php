35485b3346e1a24d44ab63229bab4e2fe3d31753
kristaps.taube <kristaps.taube@printful.com>
2021-04-23 22:06:21 +0300

    Logout functionality

--- public_html/src/Frontend/Routes/SiteRouteConfigurator.php
+++ public_html/src/Frontend/Routes/SiteRouteConfigurator.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace OSM\Frontend\Routes;
 
+use OSM\Frontend\Core\Builders\ResponseBuilder;
 use OSM\Frontend\Core\Middlewares\IsAuthorizedMiddleware;
 use OSM\Frontend\Modules\Site\Handlers\IndexRequestHandler;
 use OSM\Frontend\Modules\Site\Handlers\NewsRequestHandler;
 use OSM\Frontend\Modules\Site\Handlers\ProcessLoginHandler;
 use OSM\Frontend\Modules\Site\Handlers\RegisterRequestHandler;
+use OSM\Frontend\Modules\Site\Services\AuthorizationService;
+use Psr\Http\Message\ServerRequestInterface;
 use TheApp\Components\Router;
 use TheApp\Interfaces\RouterConfiguratorInterface;
 
@@ -23,5 +26,16 @@ class SiteRouteConfigurator implements RouterConfiguratorInterface
 
         # authorized routes
         $router->get('/news', NewsRequestHandler::class)->withMiddleware(IsAuthorizedMiddleware::class);
+
+
+        $router->get('/logout', function (
+            ServerRequestInterface $request,
+            AuthorizationService $authorizationService,
+            ResponseBuilder $responseBuilder
+        ) {
+            $authorizationService->logoutActiveUser();
+
+            return $responseBuilder->withRedirect('/')->build();
+        });
     }
 }
