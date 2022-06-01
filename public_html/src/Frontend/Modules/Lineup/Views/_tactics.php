a7128b5131cc9e2d92c2311f4ef3962eadcc82cf
kristaps.taube <kristaps.taube@printful.com>
2022-06-01 08:11:08 +0300

    adding tactic inputs

--- public_html/src/Frontend/Modules/Lineup/Views/_tactics.php
+++ public_html/src/Frontend/Modules/Lineup/Views/_tactics.php
@@ -11,13 +11,22 @@ use OSM\Frontend\Helpers\Html;
 ?>
 <?php Html::startForm('/lineup/save-tactics'); ?>
 <div>
-    <div><?php echo _d(Domains::DOMAIN_FRONTEND, 'Tactic') ?></div>
-    <div>
-        <?php echo Html::select('tactic', TeamLineup::getAvailableTactics(), $lineup->tactic) ?>
+    <div class="form-group">
+        <label for="tactic"><?php echo _d(Domains::DOMAIN_FRONTEND, 'Tactic') ?></label>
+        <?php echo Html::select('tactic', TeamLineup::getAvailableTactics(), $lineup->tactic, ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <label for="tactic"><?php echo _d(Domains::DOMAIN_FRONTEND, 'Passing style') ?></label>
+        <?php echo Html::select('passingStyle', TeamLineup::getAvailablePassingStyles(), $lineup->passingStyle, ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <label for="tactic"><?php echo _d(Domains::DOMAIN_FRONTEND, 'Defensive line') ?></label>
+        <?php echo Html::select('defensiveLine', TeamLineup::getAvailableDefensiveLines(), $lineup->defensiveLine, ['class' => 'form-control']) ?>
+    </div>
+    <div class="form-group">
+        <label for="tactic"><?php echo _d(Domains::DOMAIN_FRONTEND, 'Pressure') ?></label>
+        <?php echo Html::select('pressure', TeamLineup::getAvailablePressures(), $lineup->pressure, ['class' => 'form-control']) ?>
     </div>
 </div>
-
-
-
 <?php echo Html::submitButton('Save') ?>
 <?php echo Html::endForm() ?>
